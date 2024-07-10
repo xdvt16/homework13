8674d8aeea52377bdbe7f6040a5d1950526096ae
xdvt <xdvt1337@gmail.com>
2024-07-10 16:29:30 +0700

    Add files via upload

---
+++ homework13.py
@@ -0,0 +1,19 @@
+def send_email(message, recipient,*, sender = "university.help@gmail.com"):
+    valid_email_ending = (".com", ".ru", ".net")
+
+    if "@" not in sender or not sender.endswith(valid_email_ending) or "@" not in recipient or not recipient.endswith(valid_email_ending):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+
+    elif sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
